539e66698104782ab4b8d2ff305303e27c00b12a
SuperMario <44363781+Supermariozmx@users.noreply.github.com>
2018-10-29 21:28:50 +0800

    张梦雪提交作业

---
+++ 161CS_ZMENGXUE/MANAGE/MANAGE/Program.cs
@@ -0,0 +1,38 @@
+﻿using SquirrelFramework.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MANAGE
+{
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                Console.WriteLine("hello world");
+                var somebody = new User();
+                Console.WriteLine("input classid");
+                somebody.ClassId = Console.ReadLine();
+                Console.WriteLine("input classname");
+                somebody.ClassName = Console.ReadLine();
+                somebody.Name = Console.ReadLine();
+                somebody.Credits = int.Parse(Console.ReadLine());
+            somebody.Gender = bool.Parse(Console.ReadLine());
+            }
+            public class User : DomainModel
+            {
+                public string ClassId { get; set; }
+                public string ClassName { get; set; }
+                public string Name { get; set; }
+                public string ReportTo { get; set; }
+                public string GitHub { get; set; }
+                public int Credits { get; set; }
+                public bool Gender { get; set; }
+            }
+
+        }
+    
+}
