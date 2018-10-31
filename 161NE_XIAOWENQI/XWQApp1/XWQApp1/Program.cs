82aaef87bbd84e2ba960bc47bcc19ca72630674c
xiaowenqi <44365344+xiaowenqi@users.noreply.github.com>
2018-10-31 20:50:06 +0800

    xiao submit test

--- 161NE_XIAOWENQI/XWQApp1/XWQApp1/Program.cs
+++ 161NE_XIAOWENQI/XWQApp1/XWQApp1/Program.cs
@@ -1,12 +1,4 @@
-﻿using SquirrelFramework.Domain.Model;
-using SquirrelFramework.Repository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace XWQApp1
+﻿namespace XWQApp1
 {
     class Program
     {
@@ -45,18 +37,6 @@ namespace XWQApp1
         }
     }
 
-    [Collection("User")]
-    public class User : DomainModel
-    {
-        internal string name;
-
-        public string ClassId { get; set; }
-        public string ClassName { get; set; }
-        public string Name { get; set; }
-        public int Credits { get; set; }
-        public bool Gender { get; set; }
-        public string ReportTo { get; set; }
-        public string GitHub { get; set; }
-        }
+    
     
 }
