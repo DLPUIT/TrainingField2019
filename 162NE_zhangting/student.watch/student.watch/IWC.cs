2ebe88c4a4ab2615d544d88a58d645dd97a05d86
ztinghhh <zting007@outlook.com>
2018-11-02 18:50:45 +0800

    张婷提交作业

---
+++ 162NE_zhangting/student.watch/student.watch/IWC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student.watch
+{
+    class IWC
+    {
+        public void showTime()
+        {
+            Console.WriteLine("IWC 显示时间");
+        }
+        public  void showAdv()
+        {
+            Console.WriteLine("IWC 显示广告");
+        }
+    }
+}
