07724eb58bc5abff33fc3f6d537ec5d6a6d1bbf7
Zhangjack <zhangxr_159@outlook.com>
2018-11-04 15:19:47 +0800

    张鑫睿第二次作业

---
+++ 172NE_Zhangxinrui/out-of-order/out-of-order/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 乱序算法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] num = new int[10];
+            int[] Newnum = new int[10];
+            int i, r = 9;
+            int n;
+            int t;
+            Random rand = new Random();
+            for (i = 0; i < 10; i++)
+            {
+                num[i] = i;
+            }
+            for (i = 0; i < 10; i++)
+            {
+                n = rand.Next(0, r);
+                Newnum[i] = num[n];
+                t = num[n];
+                num[n] = num[r];
+                num[r] = t;
+                r--;
+            }
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("{0} ", Newnum[i]);
+            }
+            Console.Read();
+
+        }
+    }
+}
+
+
