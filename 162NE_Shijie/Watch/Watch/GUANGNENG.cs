bc392173dd6936ca6a5308a28489ac7b068c6c53
Cassandra-SJ <Cassandra.SJ@outlook.com>
2018-11-04 17:11:45 +0800

    石婕

---
+++ 162NE_Shijie/Watch/Watch/GUANGNENG.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Watch
+{
+    class GUANGNENG
+    {
+        void GangNeng() {
+            Console.WriteLine("我有我的名字");
+            Console.WriteLine("现在的时间是：×");
+            Console.WriteLine("广告一");
+            Console.WriteLine("广告二");
+
+        }
+    }
+}
---
+++ 162NE_Shijie/Watch/Watch/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Watch
+{
+    static class JO
+    {
+
+       static void Main(string[] args)
+        {
+            Console.WriteLine("机械表");
+            JIXIE jixie = new JIXIE();
+            jixie.Name();
+            jixie.Time();
+            jixie.Ad();
+            jixie.Ad_1();
+            jixie.Action();
+
+
+
+
+
+        }
+
+
+    }
+}
