eb9280a5da029f06ef3ef1f8df2fa88b736e08db
LJJ <37624485+liuhahah@users.noreply.github.com>
2018-11-04 14:53:29 +0800

    刘俊杰第二次作业

---
+++ 172CS_LJJ/second01_LJJ/second01_LJJ/BrandWatch/OMEGA.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace second01_LJJ.BrandWatch
+{
+    class OMEGA : WatchVariety.MechanicalWatch
+    {
+        public void TimeStart()
+        {
+            Console.WriteLine("开始计时");
+        }
+        public void TimeEnd()
+        {
+            Console.WriteLine("停止计时");
+        }
+        public void Interval()
+        {
+            Console.WriteLine("间隔为...");
+        }
+    }
+}
---
+++ 172CS_LJJ/second01_LJJ/second01_LJJ/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using second01_LJJ.BrandWatch;
+using second01_LJJ.WatchVariety;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace second01_LJJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Watch>()
+            {
+                new OMEGA{Name = "OMEGA" },
+                new IWC{Name = "IWC" },
+                new CASIO{Name = "CASIO" },
+                new CITIZEN{Name = "CITIZEN" },
+                new TISSOT{Name = "TISSOT" },
+                new RADO{Name = "RADO" }
+            };
+            Watch a = new Watch();
+            OMEGA b = new OMEGA();
+            TISSOT c = new TISSOT();
+            CASIO d = new CASIO();
+            MechanicalWatch e = new MechanicalWatch();
+            foreach (Watch watch in list)
+            {
+                Console.WriteLine(watch.Name);
+                a.ShowTime();
+                Console.Write(watch.Name);
+                a.ShowAd();
+                if (watch is OMEGA || watch is TISSOT)
+                {
+                    b.TimeStart();
+                    b.TimeEnd();
+                    b.Interval();
+                }
+                if (watch is CASIO)
+                {
+                    d.Future();
+                }
+                if (watch is MechanicalWatch)
+                {
+                    e.AutoWinding();
+
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
