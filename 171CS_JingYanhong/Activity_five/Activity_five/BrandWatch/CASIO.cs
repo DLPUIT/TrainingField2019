da751238448a9879a4cd2c7280de021b77a160ea
CandanceJ <xiaohongJing123@outlook.com>
2018-11-05 22:42:32 +0800

    Watch

--- 171CS_JingYanhong/Activity_five/Activity_five/BrandWatch/CASIO.cs
+++ 171CS_JingYanhong/Activity_five/Activity_five/BrandWatch/CASIO.cs
@@ -10,7 +10,10 @@ namespace Activity_five
     {
         public void AfterDay()
         {
-            Console.WriteLine( "XXX后是YYY天!");
+            int n = 0;
+            Console.WriteLine("请输入您要查询的日期距离今天的天数：");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine( n + "天之后是：" + DateTime.Now.AddDays(n));
         }
         public void ShowTime()
         {
