463bb12a3a1a8a4abd0c460a23a3df2f01e4cd4b
xiaowenqi <44365344+xiaowenqi@users.noreply.github.com>
2018-11-05 19:35:53 +0800

    肖文琪提交作业

---
+++ 161NE_XIAOWENQI/Watch/Watch/BrandWatch/TISSOT.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Watch
+{
+    public class TISSOT : QuartzWatch,IStopWatch
+    {
+        public void StartTiming()
+        {
+            throw new NotImplementedException();
+        }
+        public void StopTiming()
+        {
+            throw new NotImplementedException();
+        }
+        public void ShowStopTime()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 161NE_XIAOWENQI/Watch/Watch/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Watch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var list = new List<Watch>
+            {
+                new CASIO(),
+                new OMEGA{Name="my omega"}
+            };
+            foreach(Watch watch in list)
+            {
+                Console.WriteLine("I am"+watch.Name);
+                if(watch is TISSOT)
+                {
+                    (watch as TISSOT).StartTiming();
+                    (watch as TISSOT).StopTiming();
+                    (watch as TISSOT).ShowStopTime();
+                }
+                if (watch is OMEGA)
+                {
+                    (watch as OMEGA).StartTiming();
+                    (watch as OMEGA).StopTiming();
+                    (watch as OMEGA).ShowStopTime();
+                }
+                if (watch is CASIO)
+                {
+                    (watch as CASIO).CalDayAfterN();
+                }
+            }
+
+        }
+       //Console.ReadKey(true);
+    }
+}
