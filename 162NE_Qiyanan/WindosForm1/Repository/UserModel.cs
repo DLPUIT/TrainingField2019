c968b32cb8c611913418a9e0d7e3ced2372534c8
ike <wxy1253291481@outlook.com>
2018-11-07 00:08:18 +0800

    一半作业

---
+++ 162NE_Qiyanan/WindosForm1/Repository/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using SquirrelFramework.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+   public  class UserModel:DomainModel
+    {
+        public string classId{ get; set; }
+        public string className{ get; set; }
+        public string name { get; set; }
+        public int credits { get; set;}
+        public string gender { get; set; }
+        public string team { get; set; }
+        public string github { get; set; }
+    }
+}
