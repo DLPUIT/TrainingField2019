7138c28b3fd56d345cfe34e4e31dee07d022d9ad
hellomop <41988525+hellomop@users.noreply.github.com>
2018-11-15 19:41:27 +0800

    修改02

--- 171CS_FUQUANSHUN/doc_01/Student_Management/Program.cs
+++ 171CS_FUQUANSHUN/doc_01/Student_Management/Program.cs
@@ -10,7 +10,7 @@ namespace Student_Management
     {        
         private static void Main(string[] args)
         {
-            Console.WriteLine("Black and White Student Management");
+            Console.WriteLine("Black and White Student Management vision 1.0.0");
             Console.WriteLine();                        // 空行
            var executor = new Executor();
                 executor.ShowHelp();                  
