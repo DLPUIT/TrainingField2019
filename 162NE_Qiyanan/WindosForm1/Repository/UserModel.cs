a13645f361ab867626842e1fe085bcf6936caced
Qi Yanan <43344337+ikeAr@users.noreply.github.com>
2018-11-16 14:39:18 +0800

    。

--- 162NE_Qiyanan/WindosForm1/Repository/UserModel.cs
+++ 162NE_Qiyanan/WindosForm1/Repository/UserModel.cs
@@ -1,13 +1,15 @@
 ﻿using SquirrelFramework.Domain.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Service
 {
-   public  class UserModel:DomainModel
+    [Collection("WinFormUser")]
+    public  class UserModel:DomainModel
     {
         public string classId{ get; set; }
         public string className{ get; set; }
