9eae8c216a77ca17bf4141400eae081aaa8fecb4
Cassandra-SJ <Cassandra.SJ@outlook.com>
2018-11-20 21:52:40 +0800

    石婕

---
+++ 162NE_Shijie/Db/Db/UserInfoModel.cs
@@ -0,0 +1,21 @@
+﻿using SquirrelFramework.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+   // 三层架构，中间层
+namespace Db
+{
+    public class UserInfoModel: DomainModel
+    {
+        public string Name { get; set; }
+
+        public string Class { get; set; }
+
+        public string Credit { get; set; }
+
+        public string ID { get; set; }
+        
+    }
+}
