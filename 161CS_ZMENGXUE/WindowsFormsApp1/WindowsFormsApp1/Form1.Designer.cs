4cde49fd8b9706212b60b4c8ae1613e52ed2e231
SuperMario <44363781+Supermariozmx@users.noreply.github.com>
2018-11-24 21:53:56 +0800

    winform

---
+++ 161CS_ZMENGXUE/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.displaycheckedbutton = new System.Windows.Forms.Button();
+            this.addbutton = new System.Windows.Forms.Button();
+            this.deletebutton = new System.Windows.Forms.Button();
+            this.insertbutton = new System.Windows.Forms.Button();
+            this.clearcheckedbutton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "篮球",
+            "swimming",
+            "running"});
+            this.checkedListBox1.Location = new System.Drawing.Point(3, 42);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(372, 184);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.checkedListBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.03057F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.96943F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 84F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 9F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 43F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(378, 366);
+            this.tableLayoutPanel1.TabIndex = 1;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "您的兴趣爱好是？";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 5;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.displaycheckedbutton, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.addbutton, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.deletebutton, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.insertbutton, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.clearcheckedbutton, 4, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 325);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(363, 37);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // displaycheckedbutton
+            // 
+            this.displaycheckedbutton.Location = new System.Drawing.Point(3, 3);
+            this.displaycheckedbutton.Name = "displaycheckedbutton";
+            this.displaycheckedbutton.Size = new System.Drawing.Size(66, 23);
+            this.displaycheckedbutton.TabIndex = 3;
+            this.displaycheckedbutton.Text = "显示勾选项";
+            this.displaycheckedbutton.UseVisualStyleBackColor = true;
+            this.displaycheckedbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // addbutton
+            // 
+            this.addbutton.Location = new System.Drawing.Point(75, 3);
+            this.addbutton.Name = "addbutton";
+            this.addbutton.Size = new System.Drawing.Size(66, 23);
+            this.addbutton.TabIndex = 4;
+            this.addbutton.Text = "增加条目";
+            this.addbutton.UseVisualStyleBackColor = true;
+            this.addbutton.Click += new System.EventHandler(this.addbutton_Click);
+            // 
+            // deletebutton
+            // 
+            this.deletebutton.Location = new System.Drawing.Point(147, 3);
+            this.deletebutton.Name = "deletebutton";
+            this.deletebutton.Size = new System.Drawing.Size(66, 23);
+            this.deletebutton.TabIndex = 5;
+            this.deletebutton.Text = "删除条目";
+            this.deletebutton.UseVisualStyleBackColor = true;
+            this.deletebutton.Click += new System.EventHandler(this.deletebutton_Click);
+            // 
+            // insertbutton
+            // 
+            this.insertbutton.Location = new System.Drawing.Point(219, 3);
+            this.insertbutton.Name = "insertbutton";
+            this.insertbutton.Size = new System.Drawing.Size(66, 23);
+            this.insertbutton.TabIndex = 6;
+            this.insertbutton.Text = "插入条目";
+            this.insertbutton.UseVisualStyleBackColor = true;
+            this.insertbutton.Click += new System.EventHandler(this.insertbutton_Click);
+            // 
+            // clearcheckedbutton
+            // 
+            this.clearcheckedbutton.Location = new System.Drawing.Point(291, 3);
+            this.clearcheckedbutton.Name = "clearcheckedbutton";
+            this.clearcheckedbutton.Size = new System.Drawing.Size(69, 23);
+            this.clearcheckedbutton.TabIndex = 7;
+            this.clearcheckedbutton.Text = "清除选择";
+            this.clearcheckedbutton.UseVisualStyleBackColor = true;
+            this.clearcheckedbutton.Click += new System.EventHandler(this.clearcheckedbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 366);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button displaycheckedbutton;
+        private System.Windows.Forms.Button addbutton;
+        private System.Windows.Forms.Button deletebutton;
+        private System.Windows.Forms.Button insertbutton;
+        private System.Windows.Forms.Button clearcheckedbutton;
+    }
+}
+
---
+++ 161CS_ZMENGXUE/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        { 
+            //勾选双击/单击：
+            //checkedListBox1.CheckOnClick = false;
+            checkedListBox1.CheckOnClick = true;
+            //是否按顺序
+            //checkedListBox1.Sorted = true;
+            //条目总数
+            //int count = checkedListBox1.Items.Count;
+            //多列显示  不正确!!
+            //checkedListBox1.MultiColumn = true;
+            //checkedListBox1.ColumnWidth = 1;
+        }
+   
+        private void label2_Click(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        private void addbutton_Click(object sender, EventArgs e)
+        {
+            //增加---方法一：
+            //checkedListBox1.Items.Add("跑步");
+            //checkedListBox1.Items.Add("游泳");
+            //checke dListBox1.Items.Add("瑜伽", true);
+            //增加---方法二：
+           String[] arr = new String[] { "足球", "篮球", "排球" };
+             for (int i = 0; i < arr.Count(); i++)
+           {
+               checkedListBox1.Items.Add(arr[i]);
+           }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int count = checkedListBox1.Items.Count;
+            string sb = "您的兴趣爱好是：";
+
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.GetItemChecked(i))//先判断是否被选中
+                {
+                    sb = sb + checkedListBox1.CheckedItems[i].ToString();//将选中的值取出
+                }
+            }
+            label2.Text = sb;
+
+        }
+
+        private void deletebutton_Click(object sender, EventArgs e)
+        {    //从零开始
+            checkedListBox1.Items.RemoveAt(1);
+
+        }
+
+        private void insertbutton_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Insert(1, "高尔夫");
+        }
+
+        private void clearcheckedbutton_Click(object sender, EventArgs e)
+        {
+            //方式一，测试现ClearSelected()不起作用
+           /* if (checkedListBox1.CheckedItems.Count > 0)
+            { checkedListBox1.ClearSelected(); } */
+            //方式二,获取选中元素的下标
+            
+           if (checkedListBox1.CheckedItems.Count > 0) 
+            { foreach(int index in this.checkedListBox1.CheckedIndices)
+                {
+                    checkedListBox1.SetItemChecked(index,true);
+                }
+            }
+           
+          
+        }
+    }
+}
