a236a4b1b9691cebdc29b91f1f12b9207d3b6c8c
hellomop <41988525+hellomop@users.noreply.github.com>
2019-02-16 19:11:24 +0800

    222

--- 171CS_FUQUANSHUN/doc_02/leo/WebApp/Startup.cs
+++ 171CS_FUQUANSHUN/doc_02/leo/WebApp/Startup.cs
@@ -34,7 +34,10 @@ namespace WebApp
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebbApp")
+            });
             app.UseMvc();
         }
     }
