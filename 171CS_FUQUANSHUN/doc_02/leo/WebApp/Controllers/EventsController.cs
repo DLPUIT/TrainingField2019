3183957a8d40d9277b719e50e3e2233b04751db4
hellomop <41988525+hellomop@users.noreply.github.com>
2019-02-16 18:46:58 +0800

    111

---
+++ 171CS_FUQUANSHUN/doc_02/leo/WebApp/Controllers/EventsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppCore.MainClass;
+using WebAppCore.Repositories;
+
+namespace WebAppCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private EventsRepositories repository = new EventsRepositories();
+        // GET: api/API
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/API/5
+        [HttpGet("{id}", Name = "Get")]
+        public Events Get(string id)
+        {
+            return this.repository.Get(id);
+        }
+
+        // POST: api/API
+        [HttpPost]
+        public void Post(Events value)
+        {
+            this.repository.Add(value);
+        }
+
+        // PUT: api/API/5
+        [HttpPut("{id}")]
+        public void Put(Events value)
+        {
+            this.repository.Update(value);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            this.repository.Delete(id);  
+        }
+    }
+}
---
+++ 171CS_FUQUANSHUN/doc_02/leo/WebApp/MainClass/Events.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCore.MainClass
+{
+    public class Events : DomainModel
+    {
+        public string OrganzerID { get; set; }
+        public string OrganzerDisplayName { get; set; }
+        public string TimeRange { get; set; }
+        public string Topic { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public List<CheakIn> CheakInList { get; set; }
+    }
+    public class CheakIn
+    {
+        public string ParticipatorId { get; set; }
+        public string ParticipatorDisplayName { get; set; }
+        public DateTime CheckInTime { get; set; }
+    }
+}
